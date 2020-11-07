cffca472bf6b981efaa4f8cce348f9686f2c863b
Yap Jia Hong <yapjiahong2003@gmail.com>
2020-11-07 12:15:03 +0800

    Began experimenting with a new package.

--- script.py
+++ script.py
@@ -1,20 +1,27 @@
+# Built in packages
 from time import sleep
 from datetime import datetime
-import sys
+import sys, os
+
+# Selenium, and Helium
+from helium import *
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.action_chains import ActionChains as AC
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains as AC
 
-from media_downloader import Media
 from bs4 import BeautifulSoup as BS
 
+# Custom packages
+from media_downloader import Media
+
 class Player:
-    ghostery = "D:\\Coding\\python\\web\\WebPlayer\\ghostery.xpi"
+    ghostery = os.getcwd() + "\\ghostery.xpi"
+    # ghostery = "D:\\Coding\\python\\web\\WebPlayer\\ghostery.xpi"
     opts = Options()
     opts.headless = True
     driver = webdriver.Firefox()
@@ -37,65 +44,71 @@ class Player:
         pass
 
     def Play(self):
-        video = input("Video title/link: ")
+        try:
+            self.link, boolean = self.confirm
+            self.driver.get(self.link)
 
-        if " " in video:
-            self.term = video.split(" ")
-            self.term = "+".join(self.term)
-            self.url = f"https://www.youtube.com/results?search_query={self.term}"
+        except:
+            video = input("Video title/link: ")
 
-        else:
-            self.url = f"https://www.youtube.com/results?search_query={self.term}"
+            if " " in video:
+                self.term = video.split(" ")
+                self.term = "+".join(self.term)
+                self.url = f"https://www.youtube.com/results?search_query={self.term}"
 
-        self.driver.get(self.url)
-        sleep(0.5)
+            else:
+                self.url = f"https://www.youtube.com/results?search_query={self.term}"
 
-        source = self.driver.execute_script("return document.documentElement.outerHTML")
-        soup = BS(source, "lxml")
+            self.driver.get(self.url)
+            sleep(0.5)
 
-        videoClass = "yt-simple-endpoint style-scope ytd-video-renderer"
+            source = self.driver.execute_script("return document.documentElement.outerHTML")
+            soup = BS(source, "lxml")
 
-        videos = soup.find_all('a', class_=videoClass)
+            videoClass = "yt-simple-endpoint style-scope ytd-video-renderer"
 
-        print("=="*20)
-        for index, video in enumerate(videos, start=1):
-           print(f"{index}. {video['title']}")
+            videos = soup.find_all('a', class_=videoClass)
 
-        print("=="*20)
-        select = int(input("Reference video by number: "))
+            print("=="*20)
+            for index, video in enumerate(videos, start=1):
+               print(f"{index}. {video['title']}")
 
-        vidLink = videos[select - 1]['href']
-        self.link = f"https://www.youtube.com{vidLink}"
-        self.driver.get(self.link)
+            print("=="*20)
+            select = int(input("Reference video by number: "))
 
-        VID = videos[select - 1]['title']
+            vidLink = videos[select - 1]['href']
+            self.link = f"https://www.youtube.com{vidLink}"
+            self.driver.get(self.link)
 
-        print("-" * (int(len(VID)) + len("Currently listening to: ")))
-        print(f"Currently listening to: {VID}")
-        print("-" * (int(len(VID)) + len("Currently listening to: ")))
+            VID = videos[select - 1]['title']
 
-        sleep(0.7)
-        guard = 0
-        while guard <= 80:
-            try:
-                action = AC(self.driver)
-                action.send_keys("k")
-                action.perform()
-                break
+            print("-" * (int(len(VID)) + len("Currently listening to: ")))
+            print(f"Currently listening to: {VID}")
+            print("-" * (int(len(VID)) + len("Currently listening to: ")))
 
-            except Exception as e:
-                if guard >= 50 and guard <= 70:
-                    screen = self.driver.find_element(By.XPATH, "/html/body/ytd-app/div/ytd-page-manager/ytd-watch-flexy/div[4]/div[1]/div/div[1]/div/div/div/ytd-player/div/div")
-                    screen.click()
+            sleep(0.7)
+            guard = 0
+            while guard <= 80:
+                try:
+                    action = AC(self.driver)
+                    action.send_keys("k")
+                    action.perform()
                     break
-                else:
-                    guard += 1
+
+                except Exception as e:
+                    if guard >= 50 and guard <= 70:
+                        screen = self.driver.find_element(By.XPATH, "/html/body/ytd-app/div/ytd-page-manager/ytd-watch-flexy/div[4]/div[1]/div/div[1]/div/div/div/ytd-player/div/div")
+                        screen.click()
+                        break
+                    else:
+                        guard += 1
 
     def NextVideos(self):
         source = self.driver.execute_script("return document.documentElement.outerHTML")
         soup = BS(source, "lxml")
 
         try:
+            # Side bar; With playlist
             UpNext = soup.find_all("div", class_="yt-simple-endpoint style-scope ytd-playlist-panel-video-renderer")
             print("=="*20)
             for index, item in enumerate(UpNext):
@@ -105,11 +118,11 @@ class Player:
 
             select = input("Reference video by number: ")
             vidLink = UpNext[select - 1]['href']
-            self.link = f"https://www.youtube.com{vidLink}"
+            self.control = (f"https://www.youtube.com{vidLink}", True)
             self.Play()
-            self.side = True
 
         except:
+            # Side bar; No Playlist
             UpNext = soup.find_all("div", class_="yt-simple-endpoint inline-block style-scope ytd-thumbnail")
             for index, item in enumerate(UpNext):
                 print(f"{index}. {item}")
@@ -176,6 +189,9 @@ class Player:
 
             M = Media(video=self.link, mp3=check)
             M.download()
+
+        elif command == "nv":
+            self.NextVideos()
 
     def AbsoluteExit(self):
         self.driver.close()
