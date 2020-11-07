e7242796ecb334175b97a2c578242f4318417273
Yap Jia Hong <yapjiahong2003@gmail.com>
2020-11-07 08:50:48 +0800

    Files added to .gitignore, and changes regarding flow of control was made in script.py
    .gitignore
    Added a new folder to untrack, GUI, as this is not the GUI branch there was no reason to track it.
    
    script.py
    There was an issue with the flow of control, as mentioned above. When using one of the commands from PlayerControl, it kept asking the using if they wanted to watch another video. Or it will just choose to not, ask the user, and display the previous search results. This has been solved.
    
    A few methods, namely; SkipToNextVideo, and NextVideos will be incredibly difficult to implement without a GUI. Any help will be greatly appreciated. If you wish to contribute, go ahead and find my discord tag in the README.md file, and dm me.

--- script.py
+++ script.py
@@ -1,9 +1,11 @@
 from time import sleep
+from datetime import datetime
 import sys
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains as AC
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -15,10 +17,8 @@ class Player:
     ghostery = "D:\\Coding\\python\\web\\WebPlayer\\ghostery.xpi"
     opts = Options()
     opts.headless = True
-    driver = webdriver.Firefox(options=opts)
+    driver = webdriver.Firefox()
     driver.install_addon(ghostery, temporary=False)
-
-    self.AutoPlay = False
 
     sleep(0.5)
     print("Starting session.")
@@ -33,18 +33,20 @@ class Player:
         except:
             continue
 
-    def init(self, video):
-        video = input("Channel/Video: ")
-
-        if " " in video:
-            term = video.split(" ")
-            term = "+".join(term)
-            self.url = f"https://www.youtube.com/results?search_query={term}"
-
-        else:
-            self.url = f"https://www.youtube.com/results?search_query={term}"
+    def __init__(self):
+        pass
 
     def Play(self):
+        video = input("Video title/link: ")
+
+        if " " in video:
+            self.term = video.split(" ")
+            self.term = "+".join(self.term)
+            self.url = f"https://www.youtube.com/results?search_query={self.term}"
+
+        else:
+            self.url = f"https://www.youtube.com/results?search_query={self.term}"
+
         self.driver.get(self.url)
         sleep(0.5)
 
@@ -59,20 +61,35 @@ class Player:
         for index, video in enumerate(videos, start=1):
            print(f"{index}. {video['title']}")
 
-        select = int(input("Reference video by number: "))
         print("=="*20)
+        select = int(input("Reference video by number: "))
 
         vidLink = videos[select - 1]['href']
         self.link = f"https://www.youtube.com{vidLink}"
         self.driver.get(self.link)
-        sleep(1)
-        try:
-            screen = self.driver.find_element(By.XPATH, "/html/body/ytd-app/div/ytd-page-manager/ytd-watch-flexy/div[4]/div[1]/div/div[1]/div/div/div/ytd-player/div/div")
-            screen.click()
 
-        except Exception as e:
-            print(e)
-            self.Play()
+        VID = videos[select - 1]['title']
+
+        print("-" * (int(len(VID)) + len("Currently listening to: ")))
+        print(f"Currently listening to: {VID}")
+        print("-" * (int(len(VID)) + len("Currently listening to: ")))
+
+        sleep(0.7)
+        guard = 0
+        while guard <= 80:
+            try:
+                action = AC(self.driver)
+                action.send_keys("k")
+                action.perform()
+                break
+
+            except Exception as e:
+                if guard >= 50 and guard <= 70:
+                    screen = self.driver.find_element(By.XPATH, "/html/body/ytd-app/div/ytd-page-manager/ytd-watch-flexy/div[4]/div[1]/div/div[1]/div/div/div/ytd-player/div/div")
+                    screen.click()
+                    break
+                else:
+                    guard += 1
 
     def NextVideos(self):
         source = self.driver.execute_script("return document.documentElement.outerHTML")
@@ -136,36 +153,31 @@ class Player:
     def PlayerControl(self):
         command = input("Input commands: ").lower()
 
-        if not self.autoplay:
-            if command == "p": # Play, Pause mechanism
-                screen_path = "/html/body/ytd-app/div/ytd-page-manager/ytd-watch-flexy/div[4]/div[1]/div/div[1]/div/div/div/ytd-player/div/div"
-                screen = self.driver.find_element(By.XPATH, screen_path)
-                screen.click()
+        if command == "p": # Play, Pause mechanism
+            action = AC(self.driver)
+            action.send_keys("k")
+            action.perform()
 
-            elif command == "av":
-                self.Play()
+        elif command == "av":
+            self.Play()
 
-            elif command == "r":
-                self.Replay()
+        elif command == "r":
+            self.Replay()
 
-            elif command == "q":
-                self.driver.quit()
-                sys.exit()
+        elif command == "q":
+            self.AbsoluteExit()
 
-            elif command == "dl":
-                check = input("Do you want an .mp3 of this? y/n: ").lower()
-                if check == "y":
-                    check = True
-                else:
-                    check = False
+        elif command == "dl":
+            check = input("Do you want an .mp3 of this? y/n: ").lower()
+            if check == "y":
+                check = True
+            else:
+                check = False
 
-                M = Media(video=self.link, mp3=check)
-                M.download()
+            M = Media(video=self.link, mp3=check)
+            M.download()
 
-            elif command == "nv":
-                self.NextVideos()
-        else:
-            print("You are able to skip to the next video.")
-            select = input("Would you like to? y/n: ").lower()
-            if select == 'y':
-                pass
+    def AbsoluteExit(self):
+        self.driver.close()
+        sleep(0.1)
+        sys.exit()
