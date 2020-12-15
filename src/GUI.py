5b8f17402bdc82ed866881c65fda4560d2c63a6a
Jia Hong <yapjiahong2003@gmail.com>
2020-12-15 13:05:58 +0800

    Added the source files to .gitignore by accident

---
+++ src/GUI.py
@@ -0,0 +1,85 @@
+# This is the GUI
+
+# Built in packages
+from tkinter import *
+import sys, os
+
+# Custom packages
+from player import * # To the music player
+
+class Application(Frame, Player):
+    def __init__(self, master=None):
+        Frame.__init__(self, master=None)
+        Player.__init__(self)
+        self.master = master
+
+        self.Window()
+
+    def Window(self):
+        MainTitle = Label(self.master, text="Welcome", font=("Linux Libertine G", 20))
+        MainTitle.place(x=150 , y=0)
+
+        QuitBtn = Button(self.master, text="Quit", command=self.AbsoluteExit, padx=10, fg="red", font=20)
+        QuitBtn.place(x=310, y=0)
+
+        self.EnterSongName = Label(self.master, text="Enter song name: ")
+        self.EnterSongName.place(x=50, y=50)
+
+        self.SongEntry = Entry(self.master)
+        self.SongEntry.place(x=150, y=50)
+
+        FindBtn = Button(self.master, text="Find song", command=self.Search)
+        FindBtn.place(x=280 , y=50)
+
+    def Search(self):
+        video = self.SongEntry.get()
+        Player.Play(self, video)
+        self.SelectVideo()
+
+    def SelectVideo(self):
+        ReferenceByNumber = Label(self.master, text="Reference by number: ")
+        ReferenceByNumber.place(x=50, y=90)
+
+        RefByNum = Entry(self.master, borderwidth=3)
+        RefByNum.place(x=170, y=90)
+
+        ConfSelection = Button(self.master, text="Press to confirm", command=lambda: self.GetText(RefByNum))
+        ConfSelection.place(x=300, y=90)
+
+    def GetText(self, e):
+        selector = e.get()
+        Player.SelectVideo(self, selector)
+        self.CreateControlButton()
+
+    def CreateControlButton(self):
+        PlayBtn = Button(self.master, text="Play/Pause", fg="green", command=lambda: self.PlayerControl("p"))
+        PlayBtn.place(x=50, y=120)
+
+        ReplayBtn = Button(self.master, text="Replay", fg="green", command=lambda: self.PlayerControl("r"))
+        ReplayBtn.place(x=140, y=120)
+
+        PlayDownload = Button(self.master, text="Download", fg="green", command=lambda: self.PlayerControl("dl"))
+        PlayDownload.place(x=190, y=120)
+
+        self.check = BooleanVar()
+        mp3 = Checkbutton(self.master, text="mp3", variable=self.check, onvalue=True, offvalue=False)
+        mp3.place(x=280, y=120)
+
+    def PlayerControl(self, cmd):
+        check = self.check.get()
+        Player.PlayerControl(self, command=cmd, mp3=check)
+
+    def AbsoluteExit(self):
+        Player.AbsoluteExit(self)
+        sys.exit()
+
+try:
+    master = Tk()
+    master.geometry("400x400")
+
+    App = Application(master)
+    master.mainloop()
+
+except Exception as e:
+    print(e)
+    sys.exit()
---
+++ src/player.py
@@ -0,0 +1,131 @@
+# This is the music player
+
+# Built in packages
+import sys, os
+from time import sleep
+
+# Beautiful Soup/bs4
+from bs4 import BeautifulSoup as BS
+
+# Selenium
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains as AC
+
+# Custom packages
+import video # To the video downloader
+
+class Player:
+    ghostery = os.getcwd() + "/ghostery.xpi"
+    opts = Options()
+    opts.headless = True
+    driver = webdriver.Firefox(options=opts)
+    driver.install_addon(ghostery, temporary=False)
+
+    sleep(0.5)
+    print("Starting session.")
+
+    while True:
+        try:
+            driver.switch_to_window(driver.window_handles[1])
+            driver.close()
+            driver.switch_to_window(driver.window_handles[0])
+            break
+
+        except:
+            continue
+
+    def __init__(self):
+        pass
+
+    def Play(self, video):
+        self.video = video
+        if " " in self.video:
+            self.term = self.video.split(" ")
+            self.term = "+".join(self.term)
+            self.url = f"https://www.youtube.com/results?search_query={self.term}"
+
+        else:
+            self.url = f"https://www.youtube.com/results?search_query={self.video}"
+
+        self.driver.get(self.url)
+        sleep(0.5)
+
+        source = self.driver.execute_script("return document.documentElement.outerHTML")
+        soup = BS(source, "lxml")
+
+        videoClass = "yt-simple-endpoint style-scope ytd-video-renderer"
+
+        self.videos = soup.find_all('a', class_=videoClass)
+
+        print("=="*20)
+        for index, video in enumerate(self.videos, start=1):
+           print(f"{index}. {video['title']}")
+
+        print("=="*20)
+
+    def SelectVideo(self, selector):
+        vidlink = self.videos[int(selector) - 1]['href']
+        self.vidlink = f"https://www.youtube.com{vidlink}"
+        self.driver.get(self.vidlink)
+
+        sleep(0.1)
+        action = AC(self.driver)
+        action.send_keys("k")
+        action.perform()
+
+    # Player control code begins here.
+    def Replay(self):
+        self.driver.get(self.vidlink)
+        sleep(0.1)
+        action = AC(self.driver)
+        action.send_keys("k")
+        action.perform()
+
+    def PlayerControl(self, command, mp3=False):
+        if mp3 is False:
+            if command == "p": # Play, Pause mechanism
+                action = AC(self.driver)
+                action.send_keys("k")
+                action.perform()
+
+            elif command == "av":
+                self.Play()
+
+            elif command == "r":
+                self.Replay()
+
+            elif command == "q":
+                self.AbsoluteExit()
+
+            elif command == "dl":
+                M = Media(video=self.vidlink, mp3=mp3)
+                M.download()
+
+        else:
+            if command == "p": # Play, Pause mechanism
+                action = AC(self.driver)
+                action.send_keys("k")
+                action.perform()
+
+            elif command == "av":
+                self.Play()
+
+            elif command == "r":
+                self.Replay()
+
+            elif command == "q":
+                self.AbsoluteExit()
+
+            elif command == "dl":
+                M = Media(video=self.vidlink, mp3=mp3)
+                M.download()
+
+    def AbsoluteExit(self):
+        self.driver.close()
+        sleep(0.1)
+        sys.exit()
