fe00fd5890b685a1f80c819e54ed675b2ea09674
YJH16120 <yapjiahong2003@gmail.com>
2021-03-13 17:24:34 +0800

    Audio playback added with the rodio crate.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
+use std::io;
+use std::fs::File;
+use std::io::BufReader;
+use rodio::Sink;
+
+fn player_control(mut response: String, sink: &Sink) {
+    response = response.trim().to_string();
+    if response == "p".to_string() {
+            sink.pause();
+            println!("Paused.");
+            return;
+        }
+    else {
+        sink.play();
+        println!("Playing.");
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let (_stream, stream_handle) = rodio::OutputStream::try_default().unwrap();
+
+    let file = File::open("src/audio/audio.mp3").unwrap();
+    let source = rodio::Decoder::new(BufReader::new(file)).unwrap();
+    println!("Playing song.");
+
+    let sink = Sink::try_new(&stream_handle).unwrap();
+    sink.append(source);
+
+    println!("p to pause, or play");
+
+    loop {
+        let mut response = String::new();
+        io::stdin()
+            .read_line(&mut response)
+            .unwrap();
+
+        let _ = player_control(response, &sink);
+        println!("Status is paused: {}", sink.is_paused());
+   }
 }
