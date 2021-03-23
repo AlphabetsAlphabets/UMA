41056eecaab3d66123be908d2921d5aab24df4c1
YJH16120 <yapjiahong2003@gmail.com>
2021-03-23 12:31:27 +0800

    Documentation added to style.rs, and added files to .gitignore

--- src/style.rs
+++ src/style.rs
@@ -6,12 +6,13 @@ use crossterm::{
     style::{Color, ResetColor, SetColors, Colors, Print}
 };
 
-// Easy way to setup stylized output 
 pub struct Style {
+    /// Provides an easy way to setup RGB colour codes to colourized output text
     fg: [u8; 3],
 }
 
 impl Style {
+    /// Provides an easy way to create an instance of Style
     pub fn new(fg: [u8; 3]) -> Style {
         Style {
             fg,
@@ -20,6 +21,18 @@ impl Style {
 }
 
 pub fn stylized_output(style: &Style, display_text: String) {
+    //! ### Summary
+    //! Colourizes the text about to be printed onto the screen.alloc
+    //! 
+    //! ### Detailed explanation
+    //! With the `Style` struct to setup RGB color codes, `display_text` will have the colour
+    //! that belongs to the respective RGB code in the `Style` struct
+    //! 
+    //! ### Example
+    //! ```
+    //! let shade_of_green = Style::new([67, 178, 126]);
+    //! stylized_output(&shade_of_green, "This text is in a shade of green.".to_string());
+    //! ```
     let fg = Color::Rgb { 
         r: style.fg[0],
         g: style.fg[1],
